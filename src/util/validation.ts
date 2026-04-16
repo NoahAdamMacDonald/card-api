a9e48be306020ac15a6de3c3f3d39fee40ef44b4
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-15 22:14:22 -0400

    added helper validation functions to make it easier to manage validation

--- src/util/validation.ts
+++ src/util/validation.ts
@@ -11,3 +11,46 @@ export function successResponse(message: string) {
     message
   };
 }
+
+
+//Validation Checkers
+export function validateRequired(obj: any, fields: string[]) {
+    const missing: string[] = [];
+
+    for (const field of fields) {
+        if (
+        obj[field] === undefined ||
+        obj[field] === null ||
+        obj[field] === ""
+        ) {
+        missing.push(field);
+        }
+    }
+
+    if (missing.length === 0) return null;
+
+    return {
+        type: "missing required fields",
+        fields: missing
+    };
+}
+
+export function validatePositiveNumber(field: string, value: any) {
+    if (typeof value === "number" && value > 0) return null;
+
+    return {
+        type: "Invalid Value",
+        fields: [
+        {
+            field,
+            value: String(value),
+            reason: "must be Integer greater than 0"
+        }
+        ]
+    };
+}
+
+//Helper functions
+export function collectErrors(...validators: (any | null)[]) {
+    return validators.filter((v) => v !== null);
+}
