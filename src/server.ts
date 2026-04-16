276f24793a5f58282fae194f0fe4136cc0b023c7
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-15 23:50:06 -0400

    added imports and routing to server.ts and fixed brackets

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,20 @@ import { Hono } from "hono";
 import type { ApplicableRefactorInfo } from "typescript";
 
 //import routes
+import beast from "./routes/beast";
+import relic from "./routes/relic";
+import program from "./routes/program";
+import biome from "./routes/biome";
 
 const app = new Hono();
 
 //app routing
+app.route("/api/beast", beast);
+app.route("/api/relic", relic);
+app.route("/api/program", program);
+app.route("/api/biome", biome);
 
-
-export default(
+export default{
     port: 3000,
     fetch: app.fetch
-);
+};
