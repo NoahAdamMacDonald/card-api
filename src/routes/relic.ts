82a5233c89f58d31557a4c218867a90962356d5f
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-15 21:26:03 -0400

    relic base, error handle, effects

--- src/routes/relic.ts
+++ src/routes/relic.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { db } from "../db";
+import * as relicTypes from "../types/relic";
 
 const data = new Hono();
 
@@ -13,6 +14,30 @@ data.get("/", (c)=> {
     `).all();
 
     return c.json(rows);
+});
+
+data.get("/:id", (c) => {
+    const id = Number(c.req.param("id"))
+
+    //base level
+    const base = db
+    .query<relicTypes.RelicRow, [number]>(`
+        SELECT id, name, play_cost, color, bit_effect
+        FROM relics WHERE id = ?
+    `).get(id);
+
+    //error handle
+    if (!base) {
+        return c.json({ error: "Relic not found" }, 404);
+    }
+
+    //effects
+    const effect = db
+    .query<relicTypes.RelicEffectRow, [number]>(`
+        SELECT effect_id, trigger, available
+        FROM relic_effect_triggers
+        WHERE effect_id IN (SELECT id FROM relic_effects WHERE relic_id = ?)    
+    `).all(id);
 });
 
 //POST
