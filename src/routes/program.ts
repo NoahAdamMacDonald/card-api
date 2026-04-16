ff26097cc055118e80ec2e1b4fbfb1cd620aef17
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-15 21:38:00 -0400

    working on :id for program

--- src/routes/program.ts
+++ src/routes/program.ts
@@ -16,6 +16,37 @@ data.get("/", (c)=> {
     return c.json(rows);
 });
 
+data.get("/:id", (c) => {
+    const id = Number(c.req.param("id"))
+
+    //base level
+    const base = db
+    .query<programTypes.ProgramRow, [number]>(`
+        SELECT id, name, play_cost, color, bit_effect
+        FROM programs WHERE id = ?
+    `).get(id);
+
+    //error handle
+    if (!base) {
+        return c.json({ error: "Program not found" }, 404);
+    }
+
+    //effects
+    const effects = db
+    .query<programTypes.ProgramEffectRow, [number]>(`
+        SELECT id, text FROM program_effects WHERE program_id = ?
+    `).all(id);
+
+    //triggers
+      const triggers = db
+    .query<programTypes.ProgramTriggerRow, [number]>(`
+        SELECT effect_id, trigger, available
+        FROM program_effect_triggers
+        WHERE effect_id IN (SELECT id FROM program_effects WHERE program_id = ?)
+    `).all(id);
+});
+
+
 //POST
 
 //PATCH
