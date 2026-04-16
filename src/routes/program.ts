c50361c8c0112b3aff6dac23dfca81b763fd6dea
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-15 21:34:39 -0400

    program types import added and applied to get all

--- src/routes/program.ts
+++ src/routes/program.ts
@@ -1,12 +1,13 @@
 import { Hono } from "hono";
 import { db } from "../db";
+import * as programTypes from "../types/program";
 
 const data = new Hono();
 
 //GET
 data.get("/", (c)=> {
     const rows = db
-    .query(`
+    .query<Pick<programTypes.ProgramRow, "id" | "name">, []>(`
         SELECT id, name
         FROM programs
         ORDER BY id
