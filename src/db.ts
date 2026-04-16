267ce122a486e69c9506e38a89fdc3f6ca1cf765
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-15 20:11:02 -0400

    switched to run from exec

--- src/db.ts
+++ src/db.ts
@@ -2,6 +2,4 @@ import { Database } from "bun:sqlite";
 
 export const db = new Database("/data/database.sqlite");
 
-db.exec(`
-    PRAGMA foreign_keys = ON;    
-`);
+db.run(`PRAGMA foreign_keys = ON;`);
