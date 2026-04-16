33fb99dfdc38865e6e1416cabf4b08f3dcda8263
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-15 20:21:43 -0400

    added imports for schemas and table creation

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,16 @@
 import { Database } from "bun:sqlite";
 
+import { beastSchema } from "./schema/beast";
+import { biomeSchema } from "./schema/biome";
+import { programSchema } from "./schema/program";
+import { relicSchema } from "./schema/relic";
+
 export const db = new Database("/data/database.sqlite");
 
 db.run(`PRAGMA foreign_keys = ON;`);
+
+//create tables
+db.run(beastSchema);
+db.run(biomeSchema);
+db.run(programSchema);
+db.run(relicSchema);
