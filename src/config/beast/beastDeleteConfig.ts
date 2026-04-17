705f42c1a57019f73ae96375865153c1ea7f01af
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-16 22:46:45 -0400

    delete config files added

---
+++ src/config/beast/beastDeleteConfig.ts
@@ -0,0 +1,5 @@
+export const beastDeleteConfig = {
+	table: "beasts",
+	notFoundMessage: "Beast not found",
+	successMessage: "Successfully deleted Beast",
+};
---
+++ src/config/relic/relicDeleteConfig.ts
@@ -0,0 +1,5 @@
+export const relicDeleteConfig = {
+	table: "relics",
+	notFoundMessage: "Relic not found",
+	successMessage: "Successfully deleted Relic",
+};
