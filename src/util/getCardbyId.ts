88edd7bf4d7c01e17f51138c3f094e6126515e9f
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-16 22:14:18 -0400

    GET /:id helper function

--- src/util/getCardbyId.ts
+++ src/util/getCardbyId.ts
@@ -2,5 +2,27 @@ import { db } from "../db";
 import { checkExists } from "./checkExists";
 
 export function getCardbyId(c: any, config: any) {
-    
+    const id = Number(c.req.param("id"));
+
+    const exists = checkExists(c, config.table, id, config.notFoundMessage);
+    if (!exists) return exists;
+
+    //base row
+    const base = db.query(config.base.sql).get(id);
+
+    //nested
+    const nestedResults: any = {}
+
+    for (const nested of config.nested) {
+        const rows = db.query(nested.sql).all(id);
+
+        if(nested.map) {
+            nestedResults[nested.field] = rows.map(nested.map);
+        } else {
+            nestedResults[nested.field] = rows;
+        }
+    }
+
+    //build
+    return c.json(config.format(base, nestedResults));
 }
