195743744ac5c2f77273cf36110adb4a626da040
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-17 15:35:17 -0400

    added biome GET filters

--- src/routes/biome.ts
+++ src/routes/biome.ts
@@ -19,18 +19,40 @@ const data = new Hono();
 
 //GET
 data.get("/", (c) => {
+    //Filters
+	const page = Number(c.req.query("page") ?? 1);
+	const limit = Number(c.req.query("limit") ?? 20);
+	const name = c.req.query("name") ?? null;
+	const color = c.req.query("color") ?? null;
+	const minId =
+		c.req.query("minId") !== undefined ? Number(c.req.query("minId")) : null;
+	const maxId =
+		c.req.query("maxId") !== undefined ? Number(c.req.query("maxId")) : null;
+
+	const order = c.req.query("order")?.toUpperCase() === "DESC" ? "DESC" : "ASC";
+
+	const offset = (page - 1) * limit;
+
 	const rows = db
-		.query(
-			`
+    .query(`
         SELECT id, name
         FROM biomes
-        ORDER BY id
-    `,
-		)
-		.all();
+        WHERE (?1 IS NULL OR name LIKE '%' || ?1 || '%')
+        AND (?2 IS NULL OR color = ?2)
+        AND (?3 IS NULL OR id >= ?3)
+        AND (?4 IS NULL OR id <= ?4)
+        ORDER BY id ${order}
+        LIMIT ?5 OFFSET ?6
+    `).all(name, color, minId, maxId, limit, offset);
 
-	return c.json(rows);
+	return c.json({
+		page,
+		limit,
+		count: rows.length,
+		results: rows,
+	});
 });
+
 
 data.get("/:id", (c) => getCardbyId(c, biomeGetConfig));
 
