c071618b5401d6a335070ea64c2d11337e804b20
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-16 22:57:26 -0400

    working on updateCard for Patch

---
+++ src/util/updateCard.ts
@@ -0,0 +1,7 @@
+import { db } from "../db";
+import { checkExists } from "./checkExists";
+import { errorResponse } from "./validation";
+
+export async function updateCard(c: any, config: any) {
+
+}
