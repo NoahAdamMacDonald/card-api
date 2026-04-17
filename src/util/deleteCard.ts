876a6d4bee66904da419027e885791a8226682ea
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-16 22:45:18 -0400

    deleteCard function added

--- src/util/deleteCard.ts
+++ src/util/deleteCard.ts
@@ -2,5 +2,15 @@ import { db } from "../db";
 import { checkExists } from "./checkExists";
 
 export function deleteCard(c: any, config: any) {
+    const id = Number(c.req.param("id"));
+
+    const exists = checkExists(c, config.table, id, config.notFoundMessage);
+    if (!exists || exists instanceof Response) return exists;
     
+    db.query(`DELETE FROM ${config.table} WHERE id = ?`).run(id);
+
+    return c.json({
+        message: config.successMessage,
+        success: true,
+    });
 }
