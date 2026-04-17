68f3920289af52f5f3720a2bb8a6c0b980e43f5b
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-17 15:58:56 -0400

    windsurf generated JSDoc

--- src/util/getCardbyId.ts
+++ src/util/getCardbyId.ts
@@ -2,6 +2,23 @@ import { db } from "../db";
 import { checkExists } from "./checkExists";
 
 
+/**
+ * Retrieves a card by its id from the database with the given configuration.
+ * 
+ * The configuration object should contain the following properties:
+ * - table: the name of the table to retrieve the card from
+ * - notFoundMessage: the error message to return if the card is not found
+ * - base: an object containing the SQL query to retrieve the base row and the parameters to insert
+ * - nested: an array of objects containing the following properties:
+ *   - field: the name of the field in the request body.stats object that contains the nested list
+ *   - sql: the SQL query to retrieve the nested list
+ *   - map: an optional function that takes the nested list as an argument and returns a mapped version of the list
+ * - format: an optional function that takes the base row and nested results as arguments and returns a formatted version of the card
+ * 
+ * If the card is not found, a 404 response will be returned with the notFoundMessage.
+ * 
+ * If the card is successfully retrieved, a 200 response will be returned with the formatted card.
+ */
 export function getCardbyId(c: any, config: any) {
     const id = Number(c.req.param("id"));
 
