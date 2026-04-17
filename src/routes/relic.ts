e74bb15432c44572a18fea8bddac678bc6f73918
Noah Adam MacDonald <nmacdonald2113@gmail.com>
2026-04-16 23:19:48 -0400

    relic PATCH endpoint added

--- src/routes/relic.ts
+++ src/routes/relic.ts
@@ -6,11 +6,13 @@ import * as relicTypes from "../types/relic";
 import { createCard } from "../util/createCard";
 import { getCardbyId } from "../util/getCardbyId";
 import { deleteCard } from "../util/deleteCard";
+import { updateCard } from "../util/updateCard";
 
 //config
 import { relicPostConfig } from "../config/relic/relicPostConfig";
 import { relicGetConfig } from "../config/relic/relicGetConfig";
 import { relicDeleteConfig } from "../config/relic/relicDeleteConfig";
+import { relicPatchConfig } from "../config/relic/relicPatchConfig";
 
 import {
 	successResponse,
@@ -47,6 +49,9 @@ data.get("/:id", (c) => getCardbyId(c, relicGetConfig));
 
 //POST
 data.post("/", (c) => createCard(c, relicPostConfig));
+
+//PATCH
+data.patch("/:id", (c) => updateCard(c, relicPatchConfig));
 
 //DELETE
 data.delete("/:id", (c) => deleteCard(c, relicDeleteConfig));
